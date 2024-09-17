6d15c9b89ea620900a14d570257156ac6506cfec
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2024-09-17 11:37:50 +0200

    setup front in react TS

--- backend/micro-restaurant-nestjs-public-main/gateway/src/main.ts
+++ backend/micro-restaurant-nestjs-public-main/gateway/src/main.ts
@@ -9,17 +9,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
   // Retrieve config service
   const configService = app.get(ConfigService);
-
+  
   // Starts listening for shutdown hooks
   app.enableShutdownHooks();
-
+  
   // Proxy endpoints
   const dependenciesConfig = configService.get<DependenciesConfig>('dependencies');
+  console.log(`http://${dependenciesConfig.menu_service_url_with_port}`);
+  app.use('/menu', (req, res, next) => {
+    console.log('Accès à /menu');
+    next(); // Passe au middleware suivant (dans ce cas, le proxy)
+  });
+  
   app.use('/menu', createProxyMiddleware({
-    target: `http://${dependenciesConfig.menu_service_url_with_port}`,
+    target: `http://0.0.0.0:3000/doc/menus`,
     changeOrigin: true,
   }));
   app.use('/kitchen', createProxyMiddleware({
