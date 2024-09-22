70c1119cb99896716c301f1e2a652aacd9891164
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-22 10:01:49 +0200

    Create generic back navigation page

---
+++ frontend/src/components/generics/BackNavPageGeneric.tsx
@@ -0,0 +1,47 @@
+import {ArrowBack} from "@mui/icons-material";
+import {AppBar, Avatar, Box, IconButton, Stack, Toolbar, Typography} from "@mui/material";
+import {green} from "@mui/material/colors";
+import moment from "moment";
+import React, {useEffect} from "react";
+
+interface BackNavPageGenericProps {
+    title: string;
+    readyTables: number;
+    children?: React.ReactNode;
+}
+
+export default function BackNavPageGeneric({title, readyTables, children}: Readonly<BackNavPageGenericProps>) {
+    const [date, setDate] = React.useState(moment(new Date()));
+
+    // Update the date every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(moment(new Date()));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <Box display={"flex"} flexDirection={"column"} height={"100%"} overflow={"unset"}>
+            <AppBar position={"fixed"} elevation={3}>
+                <Toolbar>
+                    <IconButton edge={"start"} aria-label={"Back arrow"} sx={{mr: 2}}>
+                        <ArrowBack fontSize={"large"}/>
+                    </IconButton>
+                    <Typography variant={"h4"} sx={{flexGrow: 1}}>{title}</Typography>
+                    <Stack alignItems={"center"}>
+                        <Typography variant={"h6"}>{date.format("DD/MM/YYYY")}</Typography>
+                        <Typography variant={"h6"}>{date.format("HH")}h{date.format("mm")}</Typography>
+                    </Stack>
+                    <IconButton edge={"end"} aria-label={"Number of ready tables"} sx={{ml: 2}}>
+                        <Avatar sx={{bgcolor: green[500]}}>{readyTables}</Avatar>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Box height={"100%"} overflow={"auto"}>
+                {children}
+            </Box>
+        </Box>
+    );
+}
--- frontend/src/pages/ReservationPage.tsx
+++ frontend/src/pages/ReservationPage.tsx
@@ -1,29 +1,5 @@
-import {ArrowBack} from "@mui/icons-material";
-import {AppBar, Avatar, Box, IconButton, Stack, Toolbar, Typography} from "@mui/material";
-import {green} from "@mui/material/colors";
-import {Outlet} from "react-router-dom";
+import BackNavPageGeneric from "../components/generics/BackNavPageGeneric.tsx";
 
 export default function ReservationPage() {
-    return (
-        <Box display={"flex"} flexDirection={"column"} height={"100%"} overflow={"unset"}>
-            <AppBar position={"fixed"} elevation={3}>
-                <Toolbar>
-                    <IconButton edge={"start"} aria-label={"Back arrow"} sx={{mr: 2}}>
-                        <ArrowBack fontSize={"large"}/>
-                    </IconButton>
-                    <Typography variant={"h4"} sx={{flexGrow: 1}}>Reservation</Typography>
-                    <Stack alignItems={"center"}>
-                        <Typography variant={"h6"}>01/01/2024</Typography>
-                        <Typography variant={"h6"}>19h00</Typography>
-                    </Stack>
-                    <IconButton edge={"end"} aria-label={"Number of ready tables"} sx={{ml: 2}}>
-                        <Avatar sx={{bgcolor: green[500]}}>1</Avatar>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            <Box height={"100%"} overflow={"auto"}>
-                <Outlet/> {/* This is where the child routes will be rendered */}
-            </Box>
-        </Box>
-    );
+    return <BackNavPageGeneric title={"Reservation"} readyTables={1}/>;
 }
