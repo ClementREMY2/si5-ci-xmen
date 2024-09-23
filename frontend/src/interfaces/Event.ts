31fd26d2b9bf5b739ba959b03ebef7965dcdc2bd
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-23 11:46:18 +0200

    Add event interface

---
+++ frontend/src/interfaces/Event.ts
@@ -0,0 +1,9 @@
+import {MenuEvent, MenuItem} from "./Menu.ts";
+
+export interface Event {
+    id: string;
+    name: string;
+    date: Date;
+    menus: MenuEvent[];
+    beverages: MenuItem[];
+}
