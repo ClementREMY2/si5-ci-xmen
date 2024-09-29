bf8a9be2f4471b5c4cf891aa70337e79d23e8777
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-29 10:12:05 +0200

    Add pay all button in home filters

--- frontend/src/components/home/TableFilters.tsx
+++ frontend/src/components/home/TableFilters.tsx
@@ -1,5 +1,5 @@
 import {Button, Chip, Stack} from "@mui/material";
-import React from "react";
+import React, {useMemo} from "react";
 import {DictionaryBoolean} from "../../interfaces/Generics.ts";
 
 interface TableFiltersProps {
@@ -22,14 +22,34 @@ export default function TableFilters({selectedEvents, setSelectedEvents, width}:
         setSelectedEvents((prevState) => ({...prevState, [eventName]: !prevState[eventName]}));
     };
 
+    const singleEvent = useMemo(() => {
+        if (selectedEvents["Aucun"]) return undefined;
+        const events = Object.entries(selectedEvents).reduce((acc, [eventName, selected]) => {
+            if (selected) acc.push(eventName);
+            return acc;
+        }, [] as string[]);
+        if (events.length !== 1) return undefined; // Want just 1 event
+        return events[0];
+    }, [selectedEvents]);
+
+    const handlePayAllClick = () => {
+        console.log("Paying all tables for", singleEvent);
+    };
+
     return (
-        <Stack direction={"row"} alignItems={"center"} spacing={2} sx={{width}}>
-            <Button variant={"contained"} onClick={() => handleAllClick(true)}>Activer tous</Button>
-            {Object.entries(selectedEvents).map(([eventName, selected]) => (
-                <Chip key={eventName} label={eventName} variant={selected ? "filled" : "outlined"} color={"primary"}
-                      onClick={() => handleEventClick(eventName)}/>
-            ))}
-            <Button variant={"contained"} onClick={() => handleAllClick(false)}>Activer aucun</Button>
+        <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} spacing={3} sx={{width}}>
+            <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                <Button variant={"contained"} onClick={() => handleAllClick(true)}>Activer tous</Button>
+                {Object.entries(selectedEvents).map(([eventName, selected]) => (
+                    <Chip key={eventName} label={eventName} variant={selected ? "filled" : "outlined"} color={"primary"}
+                          onClick={() => handleEventClick(eventName)}/>
+                ))}
+                <Button variant={"contained"} onClick={() => handleAllClick(false)}>Activer aucun</Button>
+            </Stack>
+            {singleEvent &&
+                <Button variant={"contained"} onClick={handlePayAllClick} sx={{width: 150}}>
+                    Payer toutes
+                </Button>}
         </Stack>
     );
 }
