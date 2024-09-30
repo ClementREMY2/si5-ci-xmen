2803268316483da1c2a08d01bdb759e50081158b
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-30 02:55:36 +0200

    Change PaymentPage for new version (with events)

--- frontend/src/pages/PaymentPage.tsx
+++ frontend/src/pages/PaymentPage.tsx
@@ -1,175 +1,162 @@
-import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-import {Box, Button, Checkbox, FormControlLabel, Grid2, IconButton, Typography} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
+import {toast} from "react-toastify";
+import ActionCardGeneric from "../components/generics/ActionCardGeneric.tsx";
 import BackNavPageGeneric from "../components/generics/BackNavPageGeneric.tsx";
+import PaymentList from "../components/payment/PaymentList.tsx";
+import {Event} from "../interfaces/Event.ts";
+import {MenuEvent, MenuItem} from "../interfaces/Menu.ts";
+import {Order} from "../interfaces/Order.ts";
+import {Table, TableStatusEnum} from "../interfaces/Table.ts";
+import {eventsMock} from "../mocks/Event.ts";
+import {menuNormalMock} from "../mocks/Menu.ts";
+import {eventOrderMock, tableOrderMock} from "../mocks/Order.ts";
+import {tablesMock} from "../mocks/Tables.ts";
+import {checkEventName, checkTableNumber} from "../utils/PageUtils.ts";
 import {privateRoutes} from "../utils/Routes.ts";
+
+const findTable = (tableNumber?: string): Table | undefined => {
+    if (!checkTableNumber(tableNumber, false)) return undefined;
+    return tablesMock.find(table => table.table === parseFloat(tableNumber!));
+};
+
+const findEvent = (eventName?: string): Event | undefined => {
+    if (!checkEventName(eventName, false)) return undefined;
+    return eventsMock.find(event => event.name === eventName);
+};
+
+const getNbReadyTables = () => tablesMock.filter(table => table.status === TableStatusEnum.ORDER_READY).length;
 
 export default function PaymentPage() {
     const navigate = useNavigate();
-    const {table} = useParams();
+    const {table: tableNumber, event: eventName} = useParams();
+
+    const table = findTable(tableNumber);
+    const event = findEvent(eventName);
 
     useEffect(() => {
-        if (table === undefined || isNaN(parseFloat(table))) {
-            console.warn("No table specified, redirecting to home page");
+        if (!table && !event) {
+            console.warn("No table found for table number:", tableNumber, "and no event found for name:", eventName,
+                "=> redirecting to home page");
+            navigate(privateRoutes.home);
+        } else if (table && event) {
+            console.warn("Can't have table and event for payment, redirecting to home page");
             navigate(privateRoutes.home);
         }
-    }, [navigate, table]);
+    }, [navigate, tableNumber, table, eventName, event]);
 
+    const order = table ? tableOrderMock : eventOrderMock;
+    const [paying, setPaying] = useState<Order>({
+        total: 0,
+        items: {},
+        itemsEvent: {}
+    });
+    const [paid, setPaid] = useState<Order>({
+        total: 0,
+        items: {},
+        itemsEvent: {}
+    });
     const [tip, setTip] = useState<number>(0);
-    const [totalItemsPrice, setTotalItemsPrice] = useState<number>(0);
-    const [maxTotalPrice, setMaxTotalPrice] = useState<number>(0);
-    const [isTotalChecked, setIsTotalChecked] = useState<boolean>(false);
-    const [totalToDisplay, setTotalToDisplay] = useState<number>(0);
 
-    const [items, setItems] = useState([
-        { name: 'Coca-cola', price: 3, remaining: 2, ordered: 2 },
-        { name: 'Ice Tea', price: 3, remaining: 1, ordered: 1 },
-        { name: 'Orangina', price: 3, remaining: 1, ordered: 1 },
-        { name: 'Salade César', price: 7, remaining: 2, ordered: 2 },
-        { name: 'Charcuterie', price: 8, remaining: 1, ordered: 1 },
-        { name: 'Pâtes bolognaise', price: 12, remaining: 1, ordered: 1 },
-        { name: 'Pâtes carbonara', price: 12, remaining: 1, ordered: 1 },
-        { name: 'Escalope veau', price: 14, remaining: 1, ordered: 1 },
-        { name: 'Loup + frites', price: 15, remaining: 1, ordered: 1 },
-        { name: 'Fondant chocolat', price: 7, remaining: 2, ordered: 2 },
-    ]);
-
-    const handleTipChange = (value: number) => {
-        setTip(prevTip => Math.max(0, prevTip + value));
+    const getItem = (id: string, isEventMenu: boolean): MenuItem | MenuEvent | undefined => {
+        if (isEventMenu) return event?.menus.find(menu => menu.id === id);
+        if (event) return event?.beverages.find(item => item.id === id);
+        return menuNormalMock.find(item => item.id === id);
     };
 
-    const calculateMaxTotal = () => {
-        const maxTotal = items.reduce((sum, item) => sum + (item.price * item.ordered), 0);
-        setMaxTotalPrice(maxTotal);
+    const getNewQuantity = (id: string, delta: number, isEventMenu: boolean): number => {
+        const items = isEventMenu ? paying.itemsEvent! : paying.items!;
+        return (items[id] || 0) + delta;
     };
 
-    useEffect(() => {
-        calculateMaxTotal();
-    }, []);
-
-    useEffect(() => {
-        const total = items.reduce((sum, item) => sum + (item.price * item.remaining), 0);
-        setTotalItemsPrice(total);
-
-        if (isTotalChecked) {
-            setTotalToDisplay(maxTotalPrice);
+    const changePayingQuantity = (id: string, delta: number, isEventMenu: boolean = false) => {
+        // Find the item in the menu
+        const item = getItem(id, isEventMenu);
+        if (!item) {
+            console.warn(`Cannot find item with id: ${id}`);
+            return;
         }
-    }, [items, isTotalChecked, maxTotalPrice]);
 
-    const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const isChecked = event.target.checked;
-        setIsTotalChecked(isChecked);
-
-        if (isChecked) {
-            setTotalToDisplay(maxTotalPrice);
-        } else {
-            setTotalToDisplay(0);
-            setItems(prevItems => prevItems.map(item => ({ ...item, remaining: item.ordered })));
+        // Update the quantity
+        const newQuantity = getNewQuantity(id, delta, isEventMenu);
+        if (newQuantity < 0) {
+            console.warn(`Cannot have negative quantity: ${item.fullName} = ${newQuantity}`);
+            return;
         }
+
+        // Update the order
+        const newPaying = {...paying, total: paying.total + item.price * delta};
+        if (isEventMenu) newPaying.itemsEvent = {...paying.itemsEvent, [id]: newQuantity};
+        else newPaying.items = {...paying.items, [id]: newQuantity};
+        setPaying(newPaying);
     };
 
-    const handleItemQuantityChange = (index: number, change: number) => {
-        if (isTotalChecked) return;
+    const addAllToPaying = (checked: boolean) => {
+        if (!checked) {
+            removeAllFromPaying();
+            return;
+        }
 
-        setItems(prevItems => {
-            return prevItems.map((item, idx) => {
-                if (idx === index) {
-                    const newRemaining = Math.max(0, Math.min(item.remaining + change, item.ordered));
-                    if (change > 0) {
-                        const priceAdjustment = item.price;
-                        setTotalToDisplay(prevTotal => prevTotal - priceAdjustment);
-                    } else if (change < 0) {
-                        const priceAdjustment = item.price;
-                        setTotalToDisplay(prevTotal => prevTotal + priceAdjustment);
-                    }
-                    return { ...item, remaining: newRemaining };
-                }
-                return item;
+        const newPaying = {...order, total: order.total - paid.total};
+        if (newPaying.items && paid.items) {
+            Object.entries(paid.items).forEach(([id, quantity]) => {
+                newPaying.items![id] -= quantity;
             });
-        });
+        }
+        if (newPaying.itemsEvent && paid.itemsEvent) {
+            Object.entries(paid.itemsEvent).forEach(([id, quantity]) => {
+                newPaying.itemsEvent![id] -= quantity;
+            });
+        }
+        setPaying(newPaying);
+    };
+
+    const removeAllFromPaying = () => {
+        setPaying({total: 0, items: {}, itemsEvent: {}});
     };
 
     const handlePayment = () => {
-        navigate(privateRoutes.home);
+        const total = paid.total + paying.total;
+        if (total === order.total) {
+            toast.success("Paiement terminé!");
+            navigate(privateRoutes.home);
+        }
+        const newPaid = {...paid, total};
+
+        // Copy the items from paying to paid
+        Object.entries(paying.items!).forEach(([id, quantity]) => {
+            if (quantity > 0) {
+                newPaid.items![id] = (newPaid.items![id] || 0) + quantity;
+            }
+        });
+        Object.entries(paying.itemsEvent!).forEach(([id, quantity]) => {
+            if (quantity > 0) {
+                newPaid.itemsEvent![id] = (newPaid.itemsEvent![id] || 0) + quantity;
+            }
+        });
+
+        setPaid(newPaid);
+        removeAllFromPaying();
     };
 
-    const totalToPay = totalToDisplay + tip;
-
-    const card = (
-        <Box sx={{ backgroundColor: '#FFB74D', height: '100vh', padding: '16px', color: 'black' }}>
-            {/* Table and Payment Info */}
-            <Box sx={{ textAlign: 'center', marginBottom: '16px' }}>
-                <Typography variant="h4" sx={{fontWeight: "bold", color: "black"}}>Table {table!}</Typography>
-                <Typography variant="h6" sx={{ color: 'black' }}>{totalToDisplay} € / {maxTotalPrice} €</Typography>
-            </Box>
-
-            {/* Payment checkbox */}
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '16px' }}>
-                <FormControlLabel
-                    control={<Checkbox checked={isTotalChecked} onChange={handleCheckboxChange} />}
-                    label="Payer le total"
-                    sx={{ color: 'black' }}
-                />
-            </Box>
-
-            {/* Items list */}
-            <Grid2 container spacing={1}>
-                {items.map((item, index) => (
-                    <Grid2 key={index} size={12}>
-                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#FFE0B2', padding: '8px', borderRadius: '4px' }}>
-                            <Box>
-                                <Typography sx={{ color: 'black' }}>{item.name}</Typography>
-                                <Typography variant="body2" sx={{ color: 'black' }}>
-                                    Restant : {isTotalChecked ? 0 : item.remaining}
-                                </Typography>
-                            </Box>
-                            <Typography sx={{ color: 'black' }}>{item.ordered}</Typography>
-                            <Box>
-                                <IconButton
-                                    size="small"
-                                    onClick={() => handleItemQuantityChange(index, 1)}
-                                    disabled={isTotalChecked || item.remaining >= item.ordered}
-                                >
-                                    <AddCircleOutlineIcon />
-                                </IconButton>
-                                <IconButton
-                                    size="small"
-                                    onClick={() => handleItemQuantityChange(index, -1)}
-                                    disabled={isTotalChecked || item.remaining <= 0}
-                                >
-                                    <RemoveCircleOutlineIcon />
-                                </IconButton>
-                            </Box>
-                        </Box>
-                    </Grid2>
-                ))}
-            </Grid2>
-
-            {/* Tip and payment */}
-            <Box sx={{ marginTop: '16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography sx={{ marginRight: '8px', color: 'black' }}>Pourboire : {tip} €</Typography>
-                    <IconButton onClick={() => handleTipChange(-1)} size="small">
-                        <RemoveCircleOutlineIcon />
-                    </IconButton>
-                    <IconButton onClick={() => handleTipChange(1)} size="small">
-                        <AddCircleOutlineIcon />
-                    </IconButton>
-                </Box>
-                <Button
-                    variant="contained"
-                    color="error"
-                    sx={{ width: '48%' }}
-                    onClick={handlePayment}
-                    disabled={totalToDisplay !== maxTotalPrice} // Désactiver le bouton si totalToDisplay != maxTotalPrice
-                >
-                    PAYER {totalToPay} €
-                </Button>
-            </Box>
+    const card: React.ReactNode = (
+        <Box display={"flex"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} height={"100%"}>
+            <ActionCardGeneric
+                title={order.table ? `Table ${order.table}` : `Événement ${order.event}`}
+                rightTitle={`${paid.total} € / ${order.total} €`}
+                mainContent={<PaymentList order={order} paying={paying} changePayingQuantity={changePayingQuantity}
+                                          addAllToPaying={addAllToPaying} paid={paid} tip={tip} changeTip={setTip}/>}
+                buttons={
+                    <Button onClick={handlePayment} variant={"contained"} sx={{width: "200px"}}>
+                        Payer {paying.total + (isNaN(tip) ? 0 : tip)} €
+                    </Button>
+                }
+                minWidth={"95%"}
+                minHeight={"95%"}
+            />
         </Box>
-    )
+    );
 
-    return <BackNavPageGeneric title={"Payment"} readyTables={1} children={card}/>;
+    return <BackNavPageGeneric title={"Paiement"} readyTables={getNbReadyTables()} children={card}/>;
 }
