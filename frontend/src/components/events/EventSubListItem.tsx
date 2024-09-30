4a4b3dba3c0cc5eac669606a371bbe0c995774b0
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-30 16:11:28 +0200

    Create event sub list item redirecting to event page

---
+++ frontend/src/components/events/EventSubListItem.tsx
@@ -0,0 +1,28 @@
+import {Visibility} from "@mui/icons-material";
+import {Divider, IconButton, ListItem, Stack, Typography} from "@mui/material";
+import moment from "moment";
+import {generatePath, useNavigate} from "react-router-dom";
+import {Event} from "../../interfaces/Event.ts";
+import {privateRoutes} from "../../utils/Routes.ts";
+
+interface EventSubListItemProps {
+    event: Event;
+}
+
+export default function EventSubListItem({event}: Readonly<EventSubListItemProps>) {
+    const navigate = useNavigate();
+
+    const handleSeeDetails = () => {
+        navigate(generatePath(privateRoutes.event, {id: event.id}));
+    };
+
+    return (<>
+        <ListItem><Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} width={"100%"}>
+            <Typography flex={2.5} fontWeight={"bold"}>{event.name}</Typography>
+            <Typography flex={1}>{event.menus.length} menus</Typography>
+            <Typography flex={1}>{moment(event.date).format("DD/MM/YYYY")}</Typography>
+            <IconButton onClick={handleSeeDetails} color={"primary"} edge={"end"}><Visibility/></IconButton>
+        </Stack></ListItem>
+        <Divider/>
+    </>);
+}
