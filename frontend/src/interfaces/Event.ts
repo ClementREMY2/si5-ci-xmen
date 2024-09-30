87717ab3b08bc6e60490dd3eff48ffc7d423c743
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-30 16:10:57 +0200

    Complete event interfaces

--- frontend/src/interfaces/Event.ts
+++ frontend/src/interfaces/Event.ts
@@ -1,9 +1,19 @@
 import {MenuEvent, MenuItem} from "./Menu.ts";
 
+export enum EventCategoryEnum {
+    TODAY = "Aujourd'hui",
+    FUTURE = "Jours suivants",
+    PAST = "Jours précédents"
+}
+
 export interface Event {
     id?: string;
     name: string;
     date: Date;
     menus: MenuEvent[];
     beverages: MenuItem[];
+}
+
+export type Events = {
+    [key in EventCategoryEnum]: Event[];
 }
