7f09a2b0ea03a3036d271f11bce0eed785d39379
Fabien Airaud <fabien.airaud@etu.unice.fr>
2024-09-30 16:12:01 +0200

    Create event list

---
+++ frontend/src/components/events/EventList.tsx
@@ -0,0 +1,39 @@
+import {EventBusy, InsertInvitation, Today} from "@mui/icons-material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import {Button, List, Stack} from "@mui/material";
+import {EventCategoryEnum, Events} from "../../interfaces/Event.ts";
+import EventListItem from "./EventListItem.tsx";
+
+interface EventListProps {
+    events: Events;
+    width?: string | number;
+}
+
+const eventList = [
+    {key: "1", category: EventCategoryEnum.TODAY, icon: <Today color={"primary"}/>},
+    {key: "2", category: EventCategoryEnum.FUTURE, icon: <InsertInvitation color={"primary"}/>},
+    {key: "3", category: EventCategoryEnum.PAST, icon: <EventBusy color={"primary"}/>}
+];
+
+export default function EventList({events, width}: Readonly<EventListProps>) {
+    return (
+        <Stack height={"100%"} overflow={"unset"} sx={{width}}>
+            <Stack direction={"row"} justifyContent={"end"} alignItems={"center"}>
+                <Button variant={"contained"} color={"primary"} endIcon={<AddCircleOutlineIcon/>}>
+                    Ajouter un événement
+                </Button>
+            </Stack>
+            <List sx={{height: "100%", overflowY: "auto"}}>
+                {eventList.map(({key, category, icon}) => (
+                    events[category].length > 0 &&
+                    <EventListItem
+                        key={key}
+                        title={category.toUpperCase()}
+                        icon={icon}
+                        eventItems={events[category]}
+                    />
+                ))}
+            </List>
+        </Stack>
+    );
+}
