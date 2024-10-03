cb2be0080e4319cddf056a2d81fb5a1fa27ba03b
Samuel <samuel.bois@etu.univ-cotedazur.fr>
2024-10-03 18:47:34 +0200

    #2: update profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,7 +1,27 @@
+import { Box, Avatar, Typography, Container } from "@mui/material";
+import React from "react";
+
 export default function ProfilePage() {
     return (
-        <div>
-            <h1>Profile Page</h1>
-        </div>
+        <Container maxWidth="sm">
+
+            
+            <Typography variant="h4" align="center" sx={{ mt: 10 }}>
+                Profil
+            </Typography>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="80vh">
+                <Avatar 
+                    alt="Profile Picture"
+                    src="https://randomuser.me/api/portraits/men/41.jpg"
+                    sx={{ width: 250, height: 250, mb: 3 }}
+                />
+                <Typography variant="h6" align="center">
+                    John Doe
+                </Typography>
+                <Typography variant="body1" align="center" color="textSecondary">
+                    Age: 32
+                </Typography>
+            </Box>
+        </Container>
     );
 }
