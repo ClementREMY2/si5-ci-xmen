3807cb89a116f5ee96f0b02318cf2a43a73d175d
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2024-10-04 13:29:27 +0200

    add OrderService.ts

---
+++ frontend/src/interfaces/payment.interface.ts
@@ -0,0 +1,5 @@
+export interface Payment {
+    table: number;
+    date: Date;
+    payment: boolean;
+}
--- frontend/src/services/OrderService.ts
+++ frontend/src/services/OrderService.ts
@@ -1,25 +1,154 @@
-import axios from 'axios';
-import { Constants } from '../utils/Constants'
-import { OrderBackend } from '../interfaces/Order';
+import { menuNormalMock } from "./../mocks/Menu";
+import axios from "axios";
+import { Payment } from "../interfaces/payment.interface";
+import { Order, OrderItems } from "../interfaces/Order";
 
-export const getTableOrders = async () => {
-        try {
-                const response = await axios.get(Constants.gatewayUrl + 'dining/tables');
-                const data = response.data;
-
-                return data.map((item: any) => ({
-                        _id: item._id,
-                        tableNumber: item.tableNumber,
-                        customersCount: item.customersCount,
-                        opened: item.opened,
-                        lines: item.lines,
-                        preparations: item.preparations,
-                        billed: item.billed,
-                      })) as OrderBackend[];
-        } catch (error) {
-                console.error('Erreur lors de la récupération des tables:', error);
-                throw error;
-        }
+function encodeObjectToBase64(obj: any): any {
+  const jsonString = JSON.stringify(obj);
+  const base64String = Buffer.from(jsonString).toString("base64");
+  const menu = {
+    shortName: new Date().toISOString(),
+    fullName: base64String,
+    price: 10,
+    category: "STARTER",
+    image: "https://null.com",
+  };
+  return menu;
 }
 
-     
+function isOrder(obj: any): obj is Order {
+  return (
+    typeof obj.table === "number" &&
+    typeof obj.event === "string" &&
+    typeof obj.datetime === "string" &&
+    typeof obj.total === "number" &&
+    typeof obj.items === "object" &&
+    typeof obj.itemsEvent === "object"
+  );
+}
+
+async function findAllOrders(): Promise<Order[]> {
+  const menuItems: Menu[] = await axios
+    .get("http://localhost:9500/menu/menus")
+    .then((response: { data: any }) => response.data)
+    .catch((error: { message: any }) => {
+      throw new Error(`Failed to fetch menus: ${error.message}`);
+    });
+
+  const fullNamesDecoded = menuItems
+    .map((menuItem) => {
+      try {
+        const decoded = Buffer.from(menuItem.fullName, "base64").toString(
+          "ascii"
+        );
+        const decodedObj = JSON.parse(decoded);
+        decodedObj.id = menuItem._id;
+        return decodedObj;
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter((decoded) => decoded !== null);
+
+  const ordersTyped = fullNamesDecoded.filter(isOrder);
+
+  return ordersTyped;
+}
+
+interface Menu {
+  _id: string;
+  fullName: string;
+  shortName: string;
+  price: number;
+  category: "STARTER" | "MAIN" | "DESSERT";
+  image: string;
+}
+
+function isPayment(obj: any): obj is Payment {
+  return (
+    typeof obj.table === "number" &&
+    typeof obj.date === "string" &&
+    typeof obj.payment === "boolean"
+  );
+}
+
+async function getLastPaymentOfTable(
+  tableNumber: number
+): Promise<Payment | null> {
+  const payments: Menu[] = await axios
+    .get("http://localhost:9500/menu/menus")
+    .then((response: { data: any }) => response.data)
+    .catch((error: { message: any }) => {
+      throw new Error(`Failed to fetch payments: ${error.message}`);
+    });
+
+  const fullNamesDecoded = payments
+    .map((payment) => {
+      try {
+        const decoded = Buffer.from(payment.fullName, "base64").toString(
+          "ascii"
+        );
+        const decodedObj = JSON.parse(decoded);
+        decodedObj.id = payment._id;
+        return decodedObj;
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter((decoded) => decoded !== null)
+    .filter(isPayment);
+
+  const lastPayment = fullNamesDecoded.reduce<Payment | null>(
+    (acc, payment) => {
+      if (payment.table == tableNumber) {
+        if (acc === null) {
+          return payment;
+        }
+        if (new Date(payment.date) > new Date(acc.date)) {
+          return payment;
+        }
+      }
+      return acc;
+    },
+    null
+  );
+
+  return lastPayment;
+}
+
+export const getTableOrders = async (tableNumber: number) => {
+  const lastPaymentOfTable = await getLastPaymentOfTable(tableNumber);
+
+  const orders = await findAllOrders();
+
+  const ordersOfTable: Order[] = orders.filter(
+    (order) =>
+      order.table == tableNumber &&
+      (!lastPaymentOfTable ||
+        (order.datetime && order.datetime > lastPaymentOfTable.date))
+  );
+  const items: OrderItems = {};
+  const itemsEvent: OrderItems = {};
+  ordersOfTable.forEach((order: Order) => {
+    if (order.items) {
+      Object.entries(order.items).forEach(([itemId, quantity]) => {
+        if (items[itemId]) {
+          items[itemId] += quantity;
+        } else {
+          items[itemId] = quantity;
+        }
+      });
+    }
+    if (order.itemsEvent) {
+      Object.entries(order.itemsEvent).forEach(([itemId, quantity]) => {
+        if (itemsEvent[itemId]) {
+          itemsEvent[itemId] += quantity;
+        } else {
+          itemsEvent[itemId] = quantity;
+        }
+      });
+    }
+  });
+
+  return { items, itemsEvent };
+};
