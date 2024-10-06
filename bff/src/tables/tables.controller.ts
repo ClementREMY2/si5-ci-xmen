2d2c31b4bd41d281c25207d32761dd7a7089512e
Samuel <samuel.bois@etu.univ-cotedazur.fr>
2024-10-07 00:01:22 +0200

    get table for bff

--- bff/src/tables/tables.controller.ts
+++ bff/src/tables/tables.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { TablesService } from './tables.service';
+import { Table } from 'src/interfaces/front/table.interface';
 
 @Controller('tables')
 export class TablesController {
     constructor(private readonly tablesService: TablesService) {}
 
-    @Get()
-    async findAll(): Promise<string> {
+    @Get('/tables')
+    async findAll(): Promise<Table[]> {
         return await this.tablesService.findAll();
+    }
+
+    @Post('/tables/' + )
+    async updateATable(): Promise<string> {
+        return await this.tablesService.update();
     }
     
 }
--- bff/src/tables/tables.service.ts
+++ bff/src/tables/tables.service.ts
@@ -1,23 +1,98 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { table } from 'console';
+import { Menu } from 'src/interfaces/back/menu.interface';
+import { TableBackend } from 'src/interfaces/back/table.interface';
+import { GenericMenuItem } from 'src/interfaces/front/menu.interfaces';
+import { Table, TableStatusEnum } from 'src/interfaces/front/table.interface';
+import { findAllMenus } from 'src/utils/encode64-utils';
 
 @Injectable()
 export class TablesService {
     private readonly tables = [];
 
-    tableFromBackToFront(backTable) {
-        return backTable // TODO: setup the map between back and front 
-    }
-    
+    // méthode tableFromBackToFront qui retourne un tableau de Table[]
+    tableFromBackToFront = async (data): Promise<Table[]> => {
+        const backendTables: TableBackend[] = this.getBackendTableFromData(data);
+        const tables: Table[] = this.transformTableData(backendTables);
+        return await this.updateTableWithInfosInMenus(tables);
+    };
 
-    async findAll(): Promise<string> {
+    getBackendTableFromData = (data): TableBackend[] => {  
+        return data.map((item: any) => ({
+            _id: item._id,
+            number: item.number,
+            taken: item.taken,
+            tableOrderId: item.tableOrderId,
+          })) as TableBackend[];
+    } 
+        
+    // Return a table from a backend table
+    transformTableData = (dto: TableBackend[]): Table[] => {
+        return dto.map((tableDto) => {
+            let status: TableStatusEnum;
+            // TODO: get the status info from the backend
+            status = tableDto.taken ? TableStatusEnum.OCCUPIED : TableStatusEnum.AVAILABLE;
+
+            const table: Table = {
+                id: "" + tableDto.number,
+                table: tableDto.number ?? 0,
+                nbPeople: 4,
+                status: status
+            };
+
+            return table;
+        });
+    };
+
+    // Update the tables with the informations stored in the backend (menus)
+    updateTableWithInfosInMenus = async (tables: Table[]): Promise<Table[]> => {
+        let menus: GenericMenuItem[] = await findAllMenus();
+
+        menus = menus.filter(m => m.fullName.charAt(0) === '_');
+
+        for (let table of tables) {
+            let copyMenus = menus;
+            copyMenus = copyMenus.filter(m => m.fullName.split("|")[1] === table.table.toString());
+            copyMenus.sort((a, b) => {
+                const shortNameA = a.shortName.split("|");
+                const shortNameB = b.shortName.split("|");
+                return parseInt(shortNameB[0]) - parseInt(shortNameA[0]);
+            });
+
+            if (copyMenus[0] != undefined) {
+                const fullName = copyMenus[0].fullName.split("|");
+
+                table.nbPeople = parseInt(fullName[2]);
+                table.event = fullName[3];
+                table.status = fullName[4] as TableStatusEnum
+            }
+        }
+        return tables;
+    };
+
+    // Return a table from a backend table
+    transformMenuData = (dto: Menu[]): GenericMenuItem[] => {
+        return dto.map((menuDto) => {
+            const table: GenericMenuItem = {
+                id: menuDto._id,
+                fullName: menuDto.fullName,
+                shortName: menuDto.shortName,
+                price: menuDto.price,
+            };
+
+            return table;
+        });
+    };
+
+    async findAll(): Promise<Table[]> {
         try {
             const response = await axios.get('http://localhost:9500/dining/tables');
-            return JSON.stringify(this.tableFromBackToFront(response.data));
+            return this.tableFromBackToFront(response.data);
         } catch (error) {
             throw new Error(`Failed to fetch tables: ${error.message}`);
         }
     }
-    
+
+
 }
