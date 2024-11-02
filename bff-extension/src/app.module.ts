2e6947ea7f3c4ffb44e56cd0e425cfcda9bd19c4
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2024-11-02 11:41:51 +0100

    add routes for tables in bff extension

--- bff-extension/src/app.module.ts
+++ bff-extension/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersController } from './orders/orders.controller';
 import { OrdersModule } from './orders/orders.module';
+import { TablesModule } from './tables/tables.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, TablesModule],
   controllers: [AppController, OrdersController],
   providers: [AppService],
 })
